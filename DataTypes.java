3672e5009bb339f5ebee59e036001b4f7b289595
Connor Donegan <connor.donegan@vtsd.com>
2022-09-09 12:27:05 -0400

    My First Commit

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+//this is a single line comment syntax
+/*
+This
+    is
+    a
+    multi
+    line
+    comment
+    syntax
+     */
+public class DataTypes {
+//Variable syntax: dataType varName = value;
+    int test_variable = 1;
+    double test_double = 2.1;
+    boolean test_boolean = true;
+}
