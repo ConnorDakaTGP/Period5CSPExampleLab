5d45bcdb8374a5e55161cb647a6beacfcec03b25
Connor Donegan <connor.donegan@vtsd.com>
2022-09-14 12:50:04 -0400

    My First Commit

--- DataTypes.java
+++ DataTypes.java
@@ -1,16 +1,16 @@
-//this is a single line comment syntax
-/*
-This
-    is
-    a
-    multi
-    line
-    comment
-    syntax
-     */
 public class DataTypes {
-//Variable syntax: dataType varName = value;
-    int test_variable = 1;
-    double test_double = 2.1;
-    boolean test_boolean = true;
+    //Variable syntax: dataType varName = value;
+    //psvm \/ (Lets things run)
+    public static void main(String args[]){
+        int test_variable = 1;
+        double test_double = 2.1;
+        boolean test_boolean = true;
+        //sop \/ (Prints things to the computer screen)
+        System.out.println("This displays text, and the next number is test variable " +test_variable);
+        // ^ is the same as System.out.print("This displays text, and the next number is test variable "0: (Enter) System.out.print(test_variable);, ln means enter
+        //my work below
+        int n1 = 1;
+        int n2 = 6;
+        System.out.print("\tFirst number is "+n1 + " Second number is " +n2 + " Sum is " +n1+n2/n1);
+    }
 }
